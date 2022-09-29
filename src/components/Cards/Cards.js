2a8311edff6d075085c26438ef15738dcf2aefb6
siam-26 <siamshahriar390@gmail.com>
2022-09-29 11:11:56 +0600

    creater header inside component

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Cards.css';
+
+const Cards = (props) => {
+    const { img, name, time } = props.card;
+    return (
+
+        <div className='card'>
+            <img src={img} alt="" />
+            <div>
+                <h4>{name}</h4>
+                <p>{time} minutes</p>
+            </div>
+            <div className='btn-div'>
+                <button className='btn-select'>Select</button>
+            </div>
+
+
+        </div>
+
+
+    );
+};
+
+export default Cards;
--- src/components/Exercises/Exercisesmain.js
+++ src/components/Exercises/Exercisesmain.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cards from '../Cards/Cards';
 import './Exercisesmain.css';
 
 const Exercisesmain = () => {
+    const [cards, setCards] = useState([]);
+    useEffect(() => {
+        fetch('fitness.json')
+            .then(res => res.json())
+            .then(data => setCards(data));
+    }, []);
     return (
-        <div>
+        <div className='container-div-main'>
+            <div className='card-info'>
 
+                {
+                    cards.map(card => <Cards
+                        key={card.id}
+                        card={card}></Cards>)
+                }
+            </div>
+
+            <div className='calculations-div'>
+                <p>gg</p>
+            </div>
         </div>
     );
 };
